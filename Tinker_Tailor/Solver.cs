46335b782b4b08b68532321ef2b9bf56a1d25b21
David Szilagyi <szilagyidav@gmail.com>
2017-09-25 19:44:49 +0200

    Solver implemented

--- Tinker_Tailor/Solver.cs
+++ Tinker_Tailor/Solver.cs
@@ -10,7 +10,43 @@ namespace Tinker_Tailor
     {
         public T[] Solve(int remove, params T[] arr)
         {
-            throw new NotImplementedException();
+            int counter = 1;
+            int index = 0;
+            T[] result = arr;
+            while (result.Length > 1)
+            {
+                if(index >= result.Length)
+                {
+                    index = 0;
+                }
+                for (int i = index; i < result.Length; i++)
+                {
+                    if (counter == remove)
+                    {
+                        counter = 1;
+                        Resize(i, ref result);
+                        break;
+                    }
+                    index++;
+                    counter++;
+                }
+            }
+            return result;
+        }
+
+        private void Resize(int index, ref T[] arr)
+        {
+            T[] temp = new T[arr.Length - 1];
+            int counter = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(i < index || index < i)
+                {
+                    temp[counter] = arr[i];
+                    counter++;
+                }
+            }
+            arr = temp;
         }
     }
 }
--- Tinker_Tailor/Test.cs
+++ Tinker_Tailor/Test.cs
@@ -17,7 +17,11 @@ namespace Tinker_Tailor
         public void TestSolution()
         {
             Assert.AreEqual(new int[] { 4 }, test1.Solve(3, 1, 2, 3, 4, 5));
-            Assert.AreEqual(new string[] { "Codeing" }, test2.Solve(3, "Codecool","Miskolc","2017","Codeing", "Programming"));
+            Assert.AreEqual(new int[] { 3 }, test1.Solve(6, 1, 2, 3, 4));
+            Assert.AreEqual(new int[] { 1 }, test1.Solve(4, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+            Assert.AreEqual(new string[] { "Coding" }, test2.Solve(3, "Codecool","Miskolc","2017","Coding", "Programming"));
+            Assert.AreEqual(new string[] { "2017" }, test2.Solve(8, "Codecool", "Miskolc", "2017", "Coding"));
+            Assert.AreEqual(new string[] { "Developer" }, test2.Solve(1, "Codecool", "Miskolc", "2017", "Coding", "Programming", "Sofware", "Developer"));
         }
 
     }
